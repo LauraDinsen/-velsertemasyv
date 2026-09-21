e3246f234c93b6835e222ed0d9f8251fb20a8916
laura Dinsen <ladi1001@stud.ek.dk>
2026-09-21 15:20:10 +0200

    goddag

---
+++ goddag.js
@@ -0,0 +1,13 @@
+let time = new Date().getHours();
+
+console.log(time);
+
+if (time >= 5 && time < 10) {
+  console.log("Godmorgen");
+} else if (time >= 10 && time < 18) {
+  console.log("Goddag");
+} else if (time >= 18 && time < 24) {
+  console.log("Godaften");
+} else {
+  console.log("Godnat");
+}
